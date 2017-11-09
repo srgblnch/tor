9fe1ced17429af7385899274a10987082dc8b461
Sergi Blanch-Torne <sblanch@cells.es>
2017-11-09 18:58:21 +0100

    encapsulate parameters

--- launch.sh
+++ launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 source scripts/options.sh
+source $1/launchparams.sh
 
 # FIXME: check the image is available in the docker images
 # FIXME: check the arguments after the firsts that are valid to be passed to run
 # FIXME: how can be preset that an image is thought to be -i or -d
 
 docker run -i --name $1 \
-	--rm \  # --restart unless-stopped \
-	--net host \  # ${@:3} \
+	$PERSISTENCY \
+	$NETWORKING \
 	debian/$1:$RELEASE
-
---
+++ torproxy/launchparams.sh
@@ -0,0 +1,2 @@
+export PERSISTENCY="--rm "  # \  # --restart unless-stopped \
+export NETWORKING="--net host"  # \  # ${@:3} \
