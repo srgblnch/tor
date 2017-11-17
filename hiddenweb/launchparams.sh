9bb5a7f45ae05204f6c129e1f67af625c10255d9
Sergi Blanch-Torne <sblanch@cells.es>
2017-11-17 20:42:48 +0100

    little test

--- hiddenweb/launchparams.sh
+++ hiddenweb/launchparams.sh
@@ -1,4 +1,4 @@
 export INTERACTIVE='-i'
 export PERSISTENCY="--rm --restart unless-stopped "
-export NETWORKING="--net host"
-export VOLUMES="-v $(pwd)/web:/web"
+export NETWORKING=""  # "--net host"
+export VOLUMES="-v $(pwd)/hiddenweb/web:/web"
